90559d805f3fd075162ee774a7e51706193ac188
Eriun Reyes <E@gmail.com>
2022-05-04 21:51:40 -0400

    first commit

---
+++ colors.py
@@ -0,0 +1,25 @@
+import random
+
+random_colors = random.randint(0,15)
+if random_colors == 1:
+    print("You got the color blue")
+elif random_colors == 2:
+    print("You got the color yellow")
+elif random_colors == 3:
+    print("You got the color red")
+elif random_colors == 4:
+    print("You got the color black")
+elif random_colors == 5:
+    print("You got the color orange")
+elif random_colors == 6:
+    print("You got the color purple")
+elif random_colors == 7:
+    print("You got the color pink")
+elif random_colors ==  8:
+    print("You got the color green")
+elif random_colors == 9:
+    print("You got the color white")
+elif random_colors == 10:
+    print("You got the color brown")
+else:
+    print("Sorry, there is not more colors for you :v")
